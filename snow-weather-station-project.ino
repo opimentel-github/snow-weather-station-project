f567e69c424071fa361026154228869bbcd5b3ce
oscar pimentel <oscarlo.pimentel@gmail.com>
2021-12-11 09:56:23 -0300

    hc

--- snow-weather-station-project.ino
+++ snow-weather-station-project.ino
@@ -3,20 +3,27 @@
 #include "DHT.h"
 #include "RTClib.h"
 
+#define STATUS_PIN 3
+#define ERROR_PIN 4
 #define DHTTYPE DHT22
 #define DHT_PIN 2
 #define SD_CD_PIN 10
-#define STATUS_PIN 3
-#define ERROR_PIN 4
+#define HC_TRIGGER_PIN 6 
+#define HC_ECHO_PIN 5
+
+float base_distance = 100.0;
 
 DHT dht_sensor(DHT_PIN, DHT22); // instanciated before SnowStation so we use pointer as construct parameter
 RTC_DS1307 rtc_clock;
 SnowStation snow_station(\
+	STATUS_PIN,\
+	ERROR_PIN,\
 	SD_CD_PIN,\
 	&dht_sensor,\
 	&rtc_clock,\
-	STATUS_PIN,\
-	ERROR_PIN\
+	HC_TRIGGER_PIN,\
+	HC_ECHO_PIN,\
+	base_distance\
 	);
 
 //############################################################
