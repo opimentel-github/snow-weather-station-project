131913d4c9b2980809102e0c2fed9536c84bfea9
oscar pimentel <oscarlo.pimentel@gmail.com>
2022-01-09 20:55:55 -0300

    sdfat

--- src/snow-station/SnowStation.cpp
+++ src/snow-station/SnowStation.cpp
@@ -27,108 +27,224 @@ SnowStation::SnowStation(int _sd_pin, int _sd_transfer_data_ledpin, int _sd_writ
 	screen = _screen;
 }
 
-SnowStation::SnowStation(void){}
-
-//############################################################
+SnowStation::SnowStation(void){
+	;
+}
 
 void SnowStation::begin(){
 	change_state(STATE_IDLE);
 	loop_counter = 0;
-	save_record_counter = 0;
+	saved_records_counter = 0;
+	copied_sds_counter = 0;
+
 	begin_ledpins();
-	sd_transfer_data_buttonpin.begin();
-	
-	internal_humidity = NAN;
-	internal_temperature = NAN;
-	external_humidity = NAN;
-	external_temperature = NAN;
-
-	begin_dht();
-	begin_hc();
-	begin_clock();
-	begin_sd();
+	begin_buttonpins();
 	begin_screen();
-
-	change_state(STATE_SENSING_OK, MAX_UNSIGNED_LONG-1);
-	
+	begin_sd();
+	change_state(STATE_SENSING_OK);
 }
 
-void SnowStation::begin_screen(){
-	screen->InitLCD(CONTRAST);
+//############################################################
+
+bool SnowStation::begin_clock(bool set_compilation_date=false){
+	delay(SHORT_DELAY);
+	DEBUG("begining clock");
+	rtc_clock->begin();
+	DEBUGLN();
+	if (set_compilation_date){
+		rtc_clock->adjust(DateTime(F(__DATE__), F(__TIME__)));
+		DEBUG("compilation_date="); DEBUGLN(__DATE__);
+		DEBUG("compilation_hour="); DEBUGLN(__TIME__);
+	}
+	return true;
 }
 
-void SnowStation::begin_ledpins(){
+bool SnowStation::begin_internal_temperature_sensor(){
+	delay(SHORT_DELAY);
+	DEBUG("begining DHT sensor");
+	dht_sensor->begin();
+	DEBUGLN();
+	return true;
+}
+
+bool SnowStation::begin_external_temperature_sensor(){
+	delay(SHORT_DELAY);
+	return false;
+}
+
+bool SnowStation::begin_wind_sensor(){
+	delay(SHORT_DELAY);
+	return false;
+}
+
+bool SnowStation::begin_rain_sensor(){
+	delay(SHORT_DELAY);
+	return false;
+}
+
+bool SnowStation::begin_snow_distance_sensor(){
+	delay(SHORT_DELAY);
+	DEBUG("begining HC sensor");
+	pinMode(hc_trigger_pin, OUTPUT);
+	pinMode(hc_echo_pin, INPUT);
+	DEBUGLN();
+	return true;
+}
+
+//############################################################
+
+bool SnowStation::begin_ledpins(){
+	delay(SHORT_DELAY);
 	sd_transfer_data_ledpin.begin();
 	sd_write_ledpin.begin();
 }
 
-void SnowStation::begin_dht(){
-	DEBUG("begining DHT sensor");
-	dht_sensor->begin();
+bool SnowStation::begin_buttonpins(){
 	delay(SHORT_DELAY);
+	sd_transfer_data_buttonpin.begin();
 }
 
-void SnowStation::begin_hc(){
-	DEBUG("begining HC sensor");
-	pinMode(hc_trigger_pin, OUTPUT);
-	pinMode(hc_echo_pin, INPUT);
+bool SnowStation::begin_sd(){
 	delay(SHORT_DELAY);
-}
-
-void SnowStation::begin_clock(){
-	DEBUG("begining clock");
-	rtc_clock->begin();
-	// rtc_clock->adjust(DateTime(F(__DATE__), F(__TIME__)));
-	// Serial.print("compilation_date="); Serial.println(__DATE__);
-	// Serial.print("compilation_hour="); Serial.println(__TIME__);
-	delay(SHORT_DELAY);
-}
-
-void SnowStation::begin_sd(){
 	DEBUG("begining SD card");
 	pinMode(sd_pin, OUTPUT); digitalWrite(sd_pin, HIGH);
 	while(!SD.begin(sd_pin)){
 		DEBUG(".");
 	}
-	DEBUG('\n');
+	DEBUGLN();
 
-	// file
+	// new file
+	delay(SHORT_DELAY);
+	update_buffer(); // mainly to update clock
 	sprintf(record_filedir, "%s/%04d%02d%02d.txt",\
 		SD_ROOTDIR,\
-		date.year(),\
-		date.month(),\
-		date.day()\
+		date_info.year,\
+		date_info.month,\
+		date_info.day\
 		);
-	Serial.print("record_filedir="); Serial.println(record_filedir);
+	DEBUG("record_filedir="); DEBUGLN(record_filedir);
 	if (!SD.exists(record_filedir)){
 		SD.mkdir(SD_ROOTDIR);
-		file = SD.open(record_filedir, FILE_WRITE);
-		sprintf(sd_buffer_text, "%s, %s, %s, %s, %s, %s, %s",\
-			"save_record_counter",\
+		File file = SD.open(record_filedir, FILE_WRITE);
+		sprintf(sd_buffer_text, "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s",\
+			"saved_records_counter",\
+			"copied_sds_counter",\
 			"state",\
 			"julian_date",\
 			"julian_day",\
 			"internal_temperature",\
 			"internal_humidity",\
+			"external_temperature",\
+			"external_humidity",\
+			"wind_speed",\
+			"wind_direction",\
+			"rain_level",\
 			"snow_distance"\
 			);
 		file.println(sd_buffer_text); // write buffer
 		file.close(); // close the file
+		DEBUG("sd_buffer_text="); DEBUGLN(sd_buffer_text);
 	}
+	return true;
+}
+
+bool SnowStation::begin_screen(){
+	delay(SHORT_DELAY);
+	screen->InitLCD(CONTRAST);
+	return true;
 }
 
 //############################################################
 
-void SnowStation::update_dht(){
-	internal_humidity = dht_sensor->readHumidity();
-	internal_temperature = dht_sensor->readTemperature();
-	if (isnan(internal_humidity) || isnan(internal_temperature)){
-		Serial.println("DHT sensor error");
-	}
+DateInfo SnowStation::get_date_data(){
 	delay(SHORT_DELAY);
+	DateInfo date_info;
+	if (!begin_clock()){
+		return date_info;
+	}
+
+	DateTime rtc_datetime = rtc_clock->now();
+	unsigned int year = rtc_datetime.year();
+	unsigned int month = rtc_datetime.month();
+	unsigned int day = rtc_datetime.day();
+	unsigned int new_year;
+	unsigned int new_month;
+	if(month<=2){
+		new_year = year-1;
+		new_month = month+12;
+	}else{
+		new_year = year;
+		new_month = month;
+	}
+	int a = new_year/100;
+	int b = 2-a+a/4;
+	unsigned long julian_date = (long)(365.25*(new_year+4716))+(int)(30.6001*(new_month+1))+day+b-1524;
+
+	unsigned int hour = rtc_datetime.hour();
+	unsigned int minute = rtc_datetime.minute();
+	unsigned int second = rtc_datetime.second();
+	float julian_day = (float)hour/24.0+(float)minute/1440.0+(float)second/86400.0-0.5;
+
+	date_info.year = year;
+	date_info.month = month;
+	date_info.day = day;
+	date_info.hour = hour;
+	date_info.minute = minute;
+	date_info.second = second;
+	date_info.julian_date = julian_date;
+	date_info.julian_day = julian_day;
+	return date_info;
 }
 
-float SnowStation::get_hc(){
+TempInfo SnowStation::get_internal_temperature_data(){
+	delay(SHORT_DELAY);
+	TempInfo temp_info;
+	if (!begin_internal_temperature_sensor()){
+		return temp_info;
+	}
+
+	temp_info.humidity = dht_sensor->readHumidity();
+	temp_info.temperature = dht_sensor->readTemperature();
+	return temp_info;
+}
+
+TempInfo SnowStation::get_external_temperature_data(){
+	delay(SHORT_DELAY);
+	TempInfo temp_info;
+	if (!begin_external_temperature_sensor()){
+		return temp_info;
+	}
+
+	return temp_info;
+}
+
+WindInfo SnowStation::get_wind_data(){
+	delay(SHORT_DELAY);
+	WindInfo wind_info;
+	if (!begin_wind_sensor()){
+		return wind_info;
+	}
+
+	return wind_info;
+}
+
+RainInfo SnowStation::get_rain_data(){
+	delay(SHORT_DELAY);
+	RainInfo rain_info;
+	if (!begin_rain_sensor()){
+		return rain_info;
+	}
+
+	return rain_info;
+}
+
+SnowInfo SnowStation::get_snow_data(){
+	delay(SHORT_DELAY);
+	SnowInfo snow_info;
+	if (!begin_snow_distance_sensor()){
+		return snow_info;
+	}
+
 	// The sensor is triggered by a HIGH pulse of 10 or more microseconds.
 	// Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
 	digitalWrite(hc_trigger_pin, LOW);
@@ -141,50 +257,34 @@ float SnowStation::get_hc(){
 	// duration is the time (in microseconds) from the sending
 	// of the ping to the reception of its echo off of an object.
 	long duration = pulseIn(hc_echo_pin, HIGH);
-
-	// estimate speed of sound from temperature:
 	float c; // speed of sound
-	float temperature = internal_temperature;
+	float temperature = internal_temp_info.temperature;
 	if(USES_FIXED_C==1){
-		c = 331.3+0.606*temperature/100;
+		c = 331.3+0.606*temperature/100; // estimate speed of sound from temperature
 	}else{
 		c = 10000.0/29.0; // Original value for c in code
 	}
-	float hc_distance = microseconds_to_centimeters(duration, c);
-	Serial.print("hc_distance="); Serial.println(hc_distance);
-	delay(SHORT_DELAY);
-	return hc_distance;
+	snow_info.distance = microseconds_to_centimeters(duration, c);
+	return snow_info;
+	// float avg_hc_distance = 0;
+	// for(int i=0; i<HC_AVG; i++){
+	// 	avg_hc_distance += get_distance_data()/HC_AVG;
+	// }
+	// snow_distance = SNOW_BASE_DISTANCE-avg_hc_distance;
+	// DEBUG("snow_distance="); DEBUGLN(snow_distance);
+	// delay(SHORT_DELAY);
 }
 
-void SnowStation::update_hc(){
-	float avg_hc_distance = 0;
-	for(int i=0; i<HC_AVG; i++){
-		avg_hc_distance += get_hc()/HC_AVG;
-	}
-	snow_distance = SNOW_BASE_DISTANCE-avg_hc_distance;
-	Serial.print("snow_distance="); Serial.println(snow_distance);
-	delay(SHORT_DELAY);
+void SnowStation::update_all_sensor_data(){
+	date_info = get_date_data();
+	internal_temp_info = get_internal_temperature_data();
+	external_temp_info = get_external_temperature_data();
+	wind_info = get_wind_data();
+	rain_info = get_rain_data();
+	snow_info = get_snow_data();
 }
 
-void SnowStation::update_date(){
-	date = rtc_clock->now();
-	int year = date.year();
-	int month = date.month();
-	int day = date.day();
-	if(month<=2){
-		year -= 1;
-		month += 12;
-	}
-	int a = year/100;
-	int b = 2-a+a/4;
-	julian_date = (long)(365.25*(year+4716))+(int)(30.6001*(month+1))+day+b-1524;
-
-	float h = date.hour();
-	float m = date.minute();
-	float s = date.second();
-	julian_day = h/24.0+m/1440.0+s/86400.0-0.5;
-	delay(SHORT_DELAY);
-}
+//############################################################
 
 void SnowStation::update_screen_line(int x, int y, String key, String value){
 	sprintf(screen_buffer_text, "%s=%s", key.c_str(), value.c_str());
@@ -194,139 +294,108 @@ void SnowStation::update_screen_line(int x, int y, String key, String value){
 void SnowStation::update_screen(){
 	screen->clrScr();
 	screen->setFont(TinyFont);
-	// screen->print("last saved", 0, SCREEN_DY*0);
-	screen->print(String(date.year())+"/"+String(date.month())+"/"+String(date.day())+" "+String(date.hour())+":"+String(date.minute())+":"+String(date.second()), 0, SCREEN_DY*0);
+	screen->print(String(date_info.year)+"/"+String(date_info.month)+"/"+String(date_info.day)+" "+String(date_info.hour)+":"+String(date_info.minute)+":"+String(date_info.second), 0, SCREEN_DY*0);
 	screen->drawLine(0, 6, 84, 6);
-	update_screen_line(0, SCREEN_DY*1, "dst", String(snow_distance));
-	update_screen_line(0, SCREEN_DY*2, "tmp (i)", String(internal_temperature));
-	update_screen_line(0, SCREEN_DY*3, "hum (i)", String(internal_humidity));
+	update_screen_line(0, SCREEN_DY*1, "dst", String(snow_info.distance));
+	update_screen_line(0, SCREEN_DY*2, "tmp (i)", String(internal_temp_info.temperature));
+	update_screen_line(0, SCREEN_DY*3, "hum (i)", String(internal_temp_info.humidity));
 	screen->update();
+}
+
+void SnowStation::update_buffer(){
+	update_all_sensor_data();
+	sprintf(sd_buffer_text, "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s",\
+		String(saved_records_counter).c_str(),\
+		String(copied_sds_counter).c_str(),\
+		String(state).c_str(),\
+		String(date_info.julian_date).c_str(),\
+		String(date_info.julian_day, NOF_DECIMALS).c_str(),\
+		String(internal_temp_info.temperature).c_str(),\
+		String(internal_temp_info.humidity).c_str(),\
+		String(external_temp_info.temperature).c_str(),\
+		String(external_temp_info.humidity).c_str(),\
+		String(wind_info.speed).c_str(),\
+		String(wind_info.direction).c_str(),\
+		String(rain_info.level).c_str(),\
+		String(snow_info.distance).c_str()\
+		);
+	DEBUG("sd_buffer_text="); DEBUGLN(sd_buffer_text);
+	update_screen();
 }
 
 //############################################################
 
-void SnowStation::fill_buffer(){
-	sprintf(sd_buffer_text, "%s, %s, %s, %s, %s, %s, %s",\
-		String(save_record_counter).c_str(),\
-		String(state).c_str(),\
-		String(julian_date).c_str(),\
-		String(julian_day, 10).c_str(),\
-		String(internal_temperature).c_str(),\
-		String(internal_humidity).c_str(),\
-		String(snow_distance).c_str()\
-		);
-	update_screen();
-}
-
 bool SnowStation::save_record(){
-	bool success = false;
-	update_date();
-	fill_buffer();
-	file = SD.open(record_filedir, FILE_WRITE);
-	Serial.print("record_filedir=");
-	Serial.println(record_filedir);
+	delay(SHORT_DELAY);
+	update_buffer();
+	File file = SD.open(record_filedir, FILE_WRITE);
+	DEBUG("record_filedir="); DEBUGLN(record_filedir);
 	if (file){
 		file.println(sd_buffer_text); // write buffer
 		file.close(); // close the file
 		sd_write_ledpin.pulse(2);
-		success = true;
 	}else{
-		change_state(STATE_INTERNAL_SD_ERROR);
+		change_state(STATE_SD_ERROR);
+		return false;
 	}
-	save_record_counter += 1;
-	delay(SHORT_DELAY);
-	return success;
+	saved_records_counter += 1;
+	return true;
+}
+
+bool SnowStation::wait_serial_response(){
+	unsigned long k = 0;
+	while (true){
+		if (Serial3.available()){
+			serial_str = Serial3.readStringUntil('\n');
+			DEBUGLN(serial_str);
+			return true;
+		}
+		k += 1;
+		DEBUGLN(k);
+		if (k>100000){
+			return false;
+		}
+	}
 }
 
 bool SnowStation::copy_files(){
-	bool success = false;
-
-	Serial3.println("--1");
-	while (true){
-		if (Serial3.available()){
-		serial_str = Serial3.readStringUntil('\n');
-		Serial.println(serial_str);
-		break;
-		// if (serial_str=="1"){
-		// 	break;
-		// }
-		}
-	}
-
 	File dir;
 	File file;
+	Serial3.println("--1");
+	bool init_success = wait_serial_response();
+	if (!init_success){
+		return false;
+	}
 	dir = SD.open("/r/");
 	while(true){
 		file = dir.openNextFile();
 		if(!file){
-			file.close();
+			file.close(); // just in case
 			break;
 		}
 		String filename = file.name();
-
-		DEBUGLN("removing file...");
 		Serial3.println("--o "+filename);
-		while (true){
-			if (Serial3.available()){
-			serial_str = Serial3.readStringUntil('\n');
-			Serial.println(serial_str);
-			break;
-			// if (serial_str=="1"){
-			// 	break;
-			// }
-			}
-		}
+		wait_serial_response();
 		while (file.available()){
-					String read_buffer = file.readStringUntil('\n');
-					Serial3.println("--w "+read_buffer);
-					while (true){
-						if (Serial3.available()){
-						serial_str = Serial3.readStringUntil('\n');
-						Serial.println(serial_str);
-						break;
-						// if (serial_str=='1'){
-						// 	break;
-						// }
-						}
-					}
-				}
+			String read_buffer = file.readStringUntil('\n');
+			sprintf(sd_copy_buffer_text, "--w %s", read_buffer.c_str());
+			Serial3.println(sd_copy_buffer_text);
+			wait_serial_response();
+		}
 		file.close();
-		dir.close();
 		Serial3.println("--c "+filename);
-		while (true){
-			if (Serial3.available()){
-			serial_str = Serial3.readStringUntil('\n');
-			Serial.println(serial_str);
-			break;
-			// if (serial_str=="1"){
-			// 	break;
-			// }
-			}
-		}
+		wait_serial_response();
 	}
+	dir.close();
 	Serial3.println("--0");
-	while (true){
-		if (Serial3.available()){
-		serial_str = Serial3.readStringUntil('\n');
-		Serial.println(serial_str);
-		break;
-		// if (serial_str=="1"){
-		// 	break;
-		// }
-		}
-	}
-	return success;
+	wait_serial_response();
+	return true;
 }
 
 //############################################################
 
-void SnowStation::print_sd_buffer(){
-	Serial.print("sd_buffer_text="); Serial.println(sd_buffer_text);
-}
-
 void SnowStation::print_info(){
-	Serial.print("state="); Serial.println(state);
-	print_sd_buffer();
+	DEBUG("state="); DEBUGLN(state);
 }
 
 //############################################################
@@ -360,15 +429,14 @@ void SnowStation::loop(){
 	add_loop_counter();
 	if(state==STATE_SENSING_OK){ // STATE
 		if(sd_transfer_data_buttonpin.get_state()){
-			change_state(STATE_READING_OK);
+			change_state(STATE_COPYING_OK);
 		}else{
 			if(loop_counter>1000){
-				update_dht();
-				update_hc();
-				if(save_record()){
+				bool record_saved = save_record();
+				if(record_saved){
 					change_state(STATE_SENSING_OK);
 				}else{
-					change_state(STATE_INTERNAL_SD_ERROR);
+					change_state(STATE_SD_ERROR);
 				}
 			}
 		}
@@ -377,25 +445,22 @@ void SnowStation::loop(){
 		delay(LONG_DELAY);
 		change_state(STATE_SENSING_OK);
 
-	}else if(state==STATE_INTERNAL_SD_ERROR){ // STATE
+	}else if(state==STATE_SD_ERROR){ // STATE
 		begin_sd();
 		change_state(STATE_SENSING_OK);
 
-	}else if(state==STATE_READING_OK){ // STATE
+	}else if(state==STATE_COPYING_OK){ // STATE
 		sd_transfer_data_ledpin.high();
 		copy_files();
 		delay(LONG_DELAY);
 		sd_transfer_data_ledpin.low();
-		change_state(STATE_READING_FINISH);
+		change_state(STATE_COPYING_FINISH);
 
-	}else if(state==STATE_READING_FINISH){ // STATE
+	}else if(state==STATE_COPYING_FINISH){ // STATE
 		if(!sd_transfer_data_buttonpin.get_state()){
+			copied_sds_counter += 1;
 			change_state(STATE_SENSING_OK);
 		}
-
-	}else if(state==STATE_EXTERNAL_SD_ERROR){ // STATE
-		begin_sd();
-		change_state(STATE_SENSING_OK);
 
 	}else{
 		;
--- src/snow-station/SnowStation.h
+++ src/snow-station/SnowStation.h
@@ -23,25 +23,23 @@
 #define DELIMITER ':'
 
 #define USES_FIXED_C 1
-#define SHORT_DELAY 50
-#define LONG_DELAY 500
 #define CONTRAST 70
 #define SCREEN_DY 9
+#define SHORT_DELAY 10
+#define LONG_DELAY 500
 
 #define MAX_UNSIGNED_LONG 4294967295
 
-#define SNOW_BASE_DISTANCE 100.0
-
+#define NOF_DECIMALS 10
 #define HC_AVG 5
 #define SD_ROOTDIR "r"
 
 #define STATE_IDLE -1
 #define STATE_SENSING_OK 0
 #define STATE_SENSOR_ERROR 1
-#define STATE_INTERNAL_SD_ERROR 2
-#define STATE_READING_OK 3
-#define STATE_READING_FINISH 4
-#define STATE_EXTERNAL_SD_ERROR 5
+#define STATE_SD_ERROR 2
+#define STATE_COPYING_OK 3
+#define STATE_COPYING_FINISH 4
 
 extern unsigned char MediumNumbers[];
 extern unsigned char BigNumbers[];
@@ -50,37 +48,75 @@ extern unsigned char TinyFont[];
 
 //############################################################
 
-class SnowStation
-{
+struct DateInfo{
+	unsigned int year;
+	unsigned int month;
+	unsigned int day;
+	unsigned int hour;
+	unsigned int minute;
+	unsigned int second;
+	unsigned long julian_date;
+	float julian_day;
+};
+
+struct TempInfo{
+	float temperature = NAN;
+	float humidity = NAN;
+};
+
+struct WindInfo{
+	float speed = NAN;
+	float direction = NAN;
+};
+
+struct RainInfo{
+	float level = NAN;
+};
+
+struct SnowInfo{
+	float distance = NAN;
+};
+
+//############################################################
+
+class SnowStation{
 	public:
 		SnowStation(int _sd_pin, int _sd_transfer_data_ledpin, int _sd_write_ledpin, int _sd_transfer_data_buttonpin, DHT* _dht_sensor, RTC_DS1307* _rtc_clock, int _hc_trigger_pin, int _hc_echo_pin, LCD5110* _screen);
 		SnowStation(void); // empty constructor
+		void begin();
 
 		// begins
-		void begin();
-		void begin_ledpins();
-		void init_loop();
-		void begin_dht();
-		void begin_hc();
-		void begin_clock();
-		void begin_sd();
-		void begin_screen();
+		bool begin_clock(bool set_compilation_date=false);
+		bool begin_internal_temperature_sensor();
+		bool begin_external_temperature_sensor();
+		bool begin_wind_sensor();
+		bool begin_rain_sensor();
+		bool begin_snow_distance_sensor();
 
+		bool begin_ledpins();
+		bool begin_buttonpins();
+		bool begin_screen();
+		bool begin_sd();
+
+		DateInfo get_date_data();
+		TempInfo get_internal_temperature_data();
+		TempInfo get_external_temperature_data();
+		WindInfo get_wind_data();
+		RainInfo get_rain_data();
+		SnowInfo get_snow_data();
+		void update_all_sensor_data();
+		
 		// updates
-		void update_dht();
-		float get_hc();
-		void update_hc();
-		void update_date();
 		void update_screen_line(int x, int y, String key, String value);
 		void update_screen();
+		void update_buffer();
 
 		// internal sd
-		void fill_buffer();
 		bool save_record();
+		bool wait_serial_response();
 		bool copy_files();
 
 		// info
-		void print_sd_buffer();
 		void print_info();
 
 		// state
@@ -105,20 +141,20 @@ class SnowStation
 
 		int state;
 		unsigned long loop_counter;
-		File file;
-		unsigned long save_record_counter;
+		unsigned long saved_records_counter;
+		unsigned long copied_sds_counter;
 		char record_filedir[20];
-		char sd_buffer_text[200];
+		char sd_buffer_text[300];
+		char sd_copy_buffer_text[300];
 		char screen_buffer_text[100];
-		float internal_humidity;
-		float internal_temperature;
-		float external_humidity;
-		float external_temperature;
-		float snow_distance;
-		DateTime date;
-		unsigned long julian_date;
-		float julian_day;
 		String serial_str;
+
+		DateInfo date_info;
+		TempInfo internal_temp_info;
+		TempInfo external_temp_info;
+		WindInfo wind_info;
+		RainInfo rain_info;
+		SnowInfo snow_info;
 };
 
 #endif
